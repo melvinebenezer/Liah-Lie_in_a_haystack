485ee12c1ba81ebf28521d9b43805df32159983c
James Melvin <melvinebenezer@gmail.com>
2024-04-09 20:58:48 +0530

    testing workflow

--- liah/utils.py
+++ liah/utils.py
@@ -34,5 +34,6 @@ eval_prompt = system_prompt + "\n" + expert_prompt
 retrieve_prompt = lie_needles[0]["question"]
 
 if __name__ == "__main__":
+    print(system_prompt)
     print(eval_prompt)
     print(expert_prompt)
